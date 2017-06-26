5c55c1b18b5b05c3764e92667b3f18835a313a34
Badal Satyarthi <bsatyarthi@gmail.com>
2017-06-26 23:58:36 +0530

    Update RUN.py

--- RUN.py
+++ RUN.py
@@ -1,22 +1,25 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+import re
+
 import requests
 from bs4 import BeautifulSoup as bs
 import csv
 
+import os.path
+my_path = os.path.abspath(os.path.dirname(__file__))
 
-def convert_to_str(ss):
-    i = 0
-    x = ""
-    while (i < len(ss)):
-        if (ss[i] == '\xc2'):
-            i += 2
-        else:
-            x += ss[i]
-            i += 1
-    return x
+#path to save the result
+path = os.path.join(my_path, "result.csv")
 
-
+#Input the Search Item:
 search_item = raw_input("Input The Search Item: ")
+
+#Input The Location
 loc = raw_input("Input The Location: ")
+
 location = ""
 for i in loc:
     if (i == " "):
@@ -28,8 +31,7 @@ for i in loc:
 
 page_number = 1
 
-base_url = "https://www.yellowpages.com/search?search_terms=" + search_item + "&geo_location_terms=" + location + "&page=" + str(
-    page_number)
+base_url = "https://www.yellowpages.com/search?search_terms=" + search_item + "&geo_location_terms=" + location + "&page=" + str(page_number)
 
 r = requests.get(base_url)
 
@@ -43,19 +45,17 @@ phone_number = []
 
 for i in range(2,30):
     item = g_data[i]
-    header.append(convert_to_str(list((item.contents[0].find_all('a')[0].text).encode('utf-8'))))
-    address.append(convert_to_str(list((item.contents[1].find_all('p', {"class": "adr"})[0].text).encode('utf-8'))))
-    phone_number.append(convert_to_str(list((item.contents[1].find_all('div', {"itemprop": "telephone"})[0].text).encode('utf-8'))))
-
-    h1 = []
-    h2 = []
-    h3 = []
+    header.append("".join(list((item.contents[0].find_all('a')[0].text).encode('utf-8'))))
+    address.append("".join(list((item.contents[1].find_all('p', {"class": "adr"})[0].text).encode('utf-8'))))
+    phone_number.append("".join(list((item.contents[1].find_all('div', {"itemprop": "telephone"})[0].text).encode('utf-8'))))
 
     info = zip(header, address, phone_number)
 
-    with open("/Users/badalsatyarthi/PycharmProjects/WEB_Scrapping/test.csv", "w") as csv_file:
+    with open(path, "w") as csv_file:
         SourceFileWriter = csv.writer(csv_file)
-
+        SourceFileWriter.writerow([search_item.upper()+", "+loc.upper(), "ADDRESS", "Phone-Number"])
+        
         for i in info:
             SourceFileWriter.writerow(list(i))
+    
     csv_file.close()
